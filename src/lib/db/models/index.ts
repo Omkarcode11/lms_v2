16a1f41f27d94cb7c62da85c666795974ea9666b
EduFlow Developer <developer@eduflow.com>
2025-12-20 17:00:00 +0530

    feat: setup database model exports

---
+++ src/lib/db/models/index.ts
@@ -0,0 +1,11 @@
+// Export all models
+export { default as User, UserRole, type IUser } from './User';
+export { default as Course, CourseStatus, type ICourse } from './Course';
+export { default as Module, type IModule } from './Module';
+export { default as Lesson, ContentType, type ILesson } from './Lesson';
+export { default as Enrollment, EnrollmentStatus, type IEnrollment } from './Enrollment';
+export { default as Progress, type IProgress } from './Progress';
+export { default as Certificate, type ICertificate } from './Certificate';
+export { default as Review, type IReview } from './Review';
+export { default as Payment, PaymentStatus, PaymentMethod, type IPayment } from './Payment';
+
